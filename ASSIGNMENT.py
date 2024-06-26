442d7f8a1836961e699c30857e7d6e569b7de425
Adedayo2719 <maryadedayo2007@gmail.com>
2024-06-26 13:54:30 +0100

    ASSIGNMENT 1
    MR&MRS. BOSE monthly budget

--- ASSIGNMENT.py
+++ ASSIGNMENT.py
@@ -1,7 +1,7 @@
-R = 1200
-G = 300
-T = 150
-E = 100
-M = 50
-monthly_expenses = (R+G+T+E+M)
+Rent = 1200
+Groceries = 300
+Transport = 150
+Entertainment = 100
+Miscellaneous_expenses = 50
+monthly_expenses = (Rent+Groceries +Transport+Entertainment+Miscellaneous_expenses)
 print(monthly_expenses) 
